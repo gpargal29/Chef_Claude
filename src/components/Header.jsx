95aed6055b87629a11c2930e2a0942952a563924
gpargal29 <pargalgunjan@gmail.com>
2025-07-07 01:14:01 +0530

    public icon update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return (       
 
         <header>
-            <img src="/Chef_Claude_icon.png" alt="Chef Claude icon" />
+            <img src="Chef_Claude_icon.png" alt="Chef Claude icon" />
             <h1>Chef Claude</h1>
         </header>
     )
